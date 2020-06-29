3900487bc1392778cced96768674902c1dda6a68
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-29 17:07:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function printString(string){
-    // console.log(string[0])
+    console.log(string[0])
     if (string.length > 1) {
         let myString = string.substring(1, string.length)
         printString(myString)
@@ -15,4 +15,16 @@ function reverseString(string){
     } else {
         return string
     }
+}
+
+function isPalindrome(string){
+    let strLength = string.length
+    string = string.toLowerCase()
+    if(strLength === 0 || strLength === 1){
+        return true
+    }
+    if(string[0] === string[strLength - 1]){
+        return isPalindrome(string.slice(1, string.length -1)) 
+    }
+    return false
 }
