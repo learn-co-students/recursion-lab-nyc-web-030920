6dafc231830006e2ea222f77b3118afa87eb09d7
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-06-29 15:59:54 -0400

    challenge 1 complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Code your solution here!
-
+function printString(string){
+    console.log(string[0])
+    if (string.length > 1) {
+        let myString = string.substring(1, string.length)
+        printString(myString)
+    } else {
+        return true
+    }
+}
